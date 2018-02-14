d931ca27c6d627a4355aac64bdfe3ea1dbe24ad5
jakipatryk <bajbartlomiej997@gmail.com>
2018-02-14 15:50:29 +0100

    Add callback function

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,8 +1,18 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const steemconnect_firebase_functions_1 = require("steemconnect-firebase-functions");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const CORS = require("cors");
+const cors = CORS({ origin: true });
 const serviceAccount = require('../serviceAccountKey.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -20,4 +30,20 @@ exports.redirect = functions.https.onRequest((req, res) => {
     const endpoint = steemconnect_firebase_functions_1.getAuthorizationUrl(clientId, redirectUri, ['login']);
     res.redirect(endpoint);
 });
+exports.callback = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        handleCallback(req)
+            .then(token => res.status(200).send({ token }))
+            .catch(err => res.status(400).send(err));
+    });
+});
+function handleCallback(req) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const code = req.query.code;
+        const accessToken = yield steemconnect_firebase_functions_1.getAccessToken(clientId, clientSecret, redirectUri, code);
+        const uid = `steemconnect:${accessToken.username}`;
+        const firebaseToken = yield steemconnect_firebase_functions_1.mintFirebaseToken(admin, uid);
+        return firebaseToken;
+    });
+}
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,14 @@
-import { getAuthorizationUrl } from 'steemconnect-firebase-functions';
+import {
+  getAuthorizationUrl,
+  getAccessToken,
+  mintFirebaseToken
+} from 'steemconnect-firebase-functions';
 
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+
+import * as CORS from 'cors';
+const cors = CORS({ origin: true });
 
 const serviceAccount = require('../serviceAccountKey.json');
 
@@ -25,3 +32,27 @@ export const redirect = functions.https.onRequest((req, res) => {
   const endpoint = getAuthorizationUrl(clientId, redirectUri, ['login']);
   res.redirect(endpoint);
 });
+
+export const callback = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    handleCallback(req)
+      .then(token => res.status(200).send({ token }))
+      .catch(err => res.status(400).send(err));
+  });
+});
+
+async function handleCallback(req) {
+  const code = req.query.code;
+
+  const accessToken = await getAccessToken(
+    clientId,
+    clientSecret,
+    redirectUri,
+    code
+  );
+  const uid = `steemconnect:${accessToken.username}`;
+
+  const firebaseToken = await mintFirebaseToken(admin, uid);
+
+  return firebaseToken;
+}
