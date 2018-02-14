028682c5ad4c7320d32ad604a79af6663206d86c
jakipatryk <bajbartlomiej997@gmail.com>
2018-02-14 23:37:43 +0100

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
@@ -13,13 +14,19 @@ import { AuthService } from './auth.service';
 
 import { environment } from './../environments/environment';
 
+const routes: Routes = [
+  { path: '', component: UserDetailsComponent },
+  { path: 'redirect', component: RedirectComponent }
+];
+
 @NgModule({
   declarations: [AppComponent, RedirectComponent, UserDetailsComponent],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule,
-    HttpClientModule
+    AngularFireAuthModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
